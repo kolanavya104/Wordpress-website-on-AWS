0174fe94e8a36b0aa51f6948140d14ef0a8a943d
Navya <navyakola104@gmail.com>
2024-10-21 16:05:11 +0530

    Create data.tf

---
+++ data.tf
@@ -0,0 +1,20 @@
+# Fetch the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
+
+# Fetch the existing key pair for EC2 SSH access
+data "aws_key_pair" "default" {
+  key_name = var.key_name
+}
