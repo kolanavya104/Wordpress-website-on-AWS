56be112a200af5a12f1272b112a61cb3a8ef7c13
Navya <navyakola104@gmail.com>
2024-10-21 16:05:42 +0530

    Create security_groups.tf

---
+++ security_groups.tf
@@ -0,0 +1,75 @@
+resource "aws_security_group" "wordpress_sg" {
+  name   = "wordpress-sg"
+  vpc_id = module.vpc.vpc_id
+
+  # Inbound rule: Allow HTTP traffic from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Inbound rule: Allow SSH from the Bastion Host subnet (10.0.1.0/24)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24"]
+  }
+
+  # Outbound rule: Allow HTTP traffic to anywhere (0.0.0.0/0)
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound rule: Allow SSH to anywhere (10.0.1.0/24)
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.1.0/24"]
+  }
+
+  # Allow all other outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "WordPress-EC2-SG"
+  }
+}
+
+
+# Security Group for RDS
+resource "aws_security_group" "rds_sg" {
+  name   = "rds-sg"
+  vpc_id = module.vpc.vpc_id
+
+  # Allow MySQL access only from the WordPress EC2 instance
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.wordpress_sg.id]  # This is good practice
+  }
+
+  # Consider restricting outbound traffic if not necessary
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "RDS-MySQL-SG"
+  }
+}
