0a10f92b1b7a78fda42eda0778598a2511af30aa
Navya <navyakola104@gmail.com>
2024-10-21 16:18:09 +0530

    Update security_groups.tf

--- security_groups.tf
+++ security_groups.tf
@@ -73,3 +73,54 @@ resource "aws_security_group" "rds_sg" {
     Name = "RDS-MySQL-SG"
   }
 }
+
+
+
+resource "aws_security_group" "bastion_sg" {
+  name   = "bastion-sg"
+  vpc_id = module.vpc.vpc_id
+
+  # Inbound rule: Allow HTTP traffic from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Inbound rule: Allow SSH from the Bastion Host subnet (10.0.1.0/24)
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.3.0/24"]
+  }
+
+  # Outbound rule: Allow HTTP traffic to anywhere (0.0.0.0/0)
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound rule: Allow SSH to anywhere (10.0.1.0/24)
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.3.0/24"]
+  }
+
+  # Allow all other outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "bastion-EC2-SG"
+  }
+}
