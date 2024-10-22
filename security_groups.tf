7946e128f7aa655970b05baf7c679ea519020040
Navya <navyakola104@gmail.com>
2024-10-22 14:25:20 +0530

    Update security_groups.tf

--- security_groups.tf
+++ security_groups.tf
@@ -10,42 +10,54 @@ resource "aws_security_group" "wordpress_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Inbound rule: Allow SSH from the Bastion Host subnet (10.0.1.0/24)
+  # Inbound rule: Allow HTTPS traffic from anywhere (optional)
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  # Inbound rule: Allow SSH access from the bastion host
   ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.1.0/24"]
+    cidr_blocks = ["10.0.1.0/32"]  # Replace with your bastion host IP
   }
-
-  # Outbound rule: Allow HTTP traffic to anywhere (0.0.0.0/0)
-  egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Outbound rule: Allow SSH to anywhere (10.0.1.0/24)
-  egress {
+  # Inbound rule: Allow SSH access from the bastion host
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.1.0/24"]
+    cidr_blocks = ["10.0.1.0/24"]  # Replace with your bastion host IP
   }
 
-  # Allow all other outbound traffic
+  # Outbound rule: Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "-1"  # This allows all outbound traffic
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+  # Outbound rule: Allow all outbound traffic
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"  # This allows all outbound traffic
+    cidr_blocks = ["10.0.1.0/24"]
+  }
+  # Outbound rule: Allow all outbound traffic
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"  # This allows all outbound traffic
+    cidr_blocks = ["10.0.1.0/32"]
+  }
   tags = {
     Name = "WordPress-EC2-SG"
   }
 }
+
 
 
 # Security Group for RDS
@@ -76,51 +88,3 @@ resource "aws_security_group" "rds_sg" {
 
 
 
-resource "aws_security_group" "bastion_sg" {
-  name   = "bastion-sg"
-  vpc_id = module.vpc.vpc_id
-
-  # Inbound rule: Allow HTTP traffic from anywhere
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Inbound rule: Allow SSH from the Bastion Host subnet (10.0.1.0/24)
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.3.0/24"]
-  }
-
-  # Outbound rule: Allow HTTP traffic to anywhere (0.0.0.0/0)
-  egress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Outbound rule: Allow SSH to anywhere (10.0.1.0/24)
-  egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.3.0/24"]
-  }
-
-  # Allow all other outbound traffic
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "bastion-EC2-SG"
-  }
-}
